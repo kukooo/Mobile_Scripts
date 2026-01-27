103ce1032a4b9ca36ca1a67a93422604f5548faa
0xKUKO <kuk000@proton.me>
2026-01-27 15:39:00 +0100

    Update table to list

--- iOS/GetIOsAppsPaths.py
+++ iOS/GetIOsAppsPaths.py
@@ -38,22 +38,19 @@ def get_installed_apps():
 if __name__ == "__main__":
     apps = get_installed_apps()
     if apps:
-        # Find max lengths for dynamic formatting
-        max_display_name_length = max(len(app.get("CFBundleDisplayName", "Unknown")) for app in apps)
-        max_version_length = max(len(app.get("CFBundleShortVersionString", "Unknown")) for app in apps)
-        max_bundle_id_length = max(len(app.get("CFBundleIdentifier", "Unknown")) for app in apps)
-        max_app_path_length = max(len(app.get("Path", "Unknown")) for app in apps)
-        max_container_path_length = max(len(app.get("Path", "Unknown")) for app in apps)
+        print(f"Found {len(apps)} installed apps:\n")
 
-        print(f"{'Display Name':<{max_display_name_length}} {'Bundle ID':<{max_bundle_id_length}} {'Version':<{max_version_length}} {'App Path':<{max_app_path_length}} {'Container Path':<{max_container_path_length}}")
-        print("=" * (max_display_name_length + max_version_length + max_bundle_id_length + max_app_path_length + max_container_path_length - 5))
-
-        for app in apps:
+        for i, app in enumerate(apps, 1):
             bundle_id = app.get("CFBundleIdentifier", "Unknown")
             display_name = app.get("CFBundleDisplayName", "Unknown")
             app_version = app.get("CFBundleShortVersionString", "Unknown")
             app_path = app.get("Path", "Unknown")
             container_path = app.get("Container", "Unknown")
 
-            print(f"{display_name:<{max_display_name_length}} {bundle_id:<{max_bundle_id_length}} {app_version:<{max_version_length}} {app_path:<{max_app_path_length}} {container_path}")
+            print(f"[{i}] {display_name}")
+            print(f"    Bundle ID:      {bundle_id}")
+            print(f"    Version:        {app_version}")
+            print(f"    App Path:       {app_path}")
+            print(f"    Container Path: {container_path}")
+            print()
 
